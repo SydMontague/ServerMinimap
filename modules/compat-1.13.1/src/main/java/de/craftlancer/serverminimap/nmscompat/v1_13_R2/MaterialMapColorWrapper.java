4c7737640d26c2bc536918f1393579e14bde5581
SydMontague <sydmontague@phoenix-staffel.de>
2018-09-27 21:35:35 +0200

    Updated to 0.7.17
    Added 1.13.1 compatibility

---
+++ modules/compat-1.13.1/src/main/java/de/craftlancer/serverminimap/nmscompat/v1_13_R2/MaterialMapColorWrapper.java
@@ -0,0 +1,20 @@
+package de.craftlancer.serverminimap.nmscompat.v1_13_R2;
+
+import net.minecraft.server.v1_13_R2.MaterialMapColor;
+import de.craftlancer.serverminimap.nmscompat.MaterialMapColorInterface;
+
+public class MaterialMapColorWrapper implements MaterialMapColorInterface
+{
+    private MaterialMapColor color;
+    
+    public MaterialMapColorWrapper(MaterialMapColor color)
+    {
+        this.color = color;
+    }
+    
+    @Override
+    public int getM()
+    {
+        return color.ac;
+    }
+}
